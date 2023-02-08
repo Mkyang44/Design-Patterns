1ec569f0d3a58e89e702d54620f4b310cb7e51ab
Michael Yang <70030989+Mkyang44@users.noreply.github.com>
2023-02-08 13:36:29 -0500

    Delete PlasticAnimalPuzzle.java

--- PlasticAnimalPuzzle.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class PlasticAnimalPuzzle extends Puzzle{
-    public PlasticAnimalPuzzle(){
-        super("Animal by Fisher Price", "plastic",
-        Arrays.asList("Fox", "Giraffe", "Elephant", "Owl", "Monkey"));
-    }
-}
