3329cc49f25f98b2eb49ff89ff53be92cf9c4eff
Michael Yang <70030989+Mkyang44@users.noreply.github.com>
2023-02-08 13:36:33 -0500

    Delete PlasticColorPuzzle.java

--- PlasticColorPuzzle.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class PlasticColorPuzzle extends Puzzle{
-    public PlasticColorPuzzle(){
-        super("plastic Color by Fisher Price", "Plastic",
-        Arrays.asList("Green dog", "Red Dog", "Blue Dog", "Yellow Dog", "Brown Dog"));
-    }
-}
