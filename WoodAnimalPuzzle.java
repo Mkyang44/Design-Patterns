23c70bcc066df37c191397ec8a98d318068a4e5e
Michael Yang <70030989+Mkyang44@users.noreply.github.com>
2023-02-08 13:36:50 -0500

    Delete WoodAnimalPuzzle.java

--- WoodAnimalPuzzle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class WoodAnimalPuzzle extends Puzzle{
-
-    public WoodAnimalPuzzle() {
-        super("Animal by Melissa and Doug", "wood", 
-        Arrays.asList("Horse", "Sheep", "Cat", "Dog", "Chicken", "Cow"));
-    }
-}
