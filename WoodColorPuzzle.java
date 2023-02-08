ea2184fe1d9bbb5647c95201c2bae682ccdc01f9
Michael Yang <70030989+Mkyang44@users.noreply.github.com>
2023-02-08 13:36:54 -0500

    Delete WoodColorPuzzle.java

--- WoodColorPuzzle.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class WoodColorPuzzle extends Puzzle{
-    public WoodColorPuzzle(){
-        super("Color by Melissa and Doug", "Wood", 
-        Arrays.asList("Red Fish", "Green Fish", "Brown Fish", "Yellow Fish", "Purple Fish", "Black Fish", "Pink Fish", "Orange Fish", "WHite Fish"));
-    }
-}
